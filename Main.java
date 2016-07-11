e0e1800971f26bb9fb4b76eb09f7365967750867
RamIowaStateUniversity <rramu@iastate.edu>
2016-07-10 20:29:45 -0500

    2

--- Main.java
+++ Main.java
@@ -6,4 +6,37 @@ public class Main {
 		a = 5;
 		d = c;
 	}
+
+	private void alias_branch() {
+		int a = b;
+		a = 5;
+		if(a==5) {
+			a = 6;
+		}
+		else {
+			a = 7;
+		}
+		d = c;
+		int c = 1;
+		int d = e;
+	}
+
+	private void alias_loop() {
+		int a = b;
+		a = 5;
+		if(a==5) {
+			a = 6;
+		}
+		else {
+			a = 7;
+		}
+		d = c;
+		int c = 1;
+		while(true) {
+			int x =6;
+			int y = 7;
+		}
+		System.out.println(a);
+		int d = e;
+	}
 }
