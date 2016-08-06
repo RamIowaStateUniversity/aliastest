bbc3dd3a8e9de9456146cc9a095754ce0d19b55f
RamIowaStateUniversity <rramu@iastate.edu>
2016-08-05 20:32:28 -0500

    write

--- Main.java
+++ Main.java
@@ -235,4 +235,21 @@ public class Main {
 		}		
 		return 1;
 	}
+
+	public void write() {
+		String content = "This is the content to write into file";
+
+		File file = new File("/users/mkyong/filename.txt");
+
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(content);
+		bw.close();
+
+		System.out.println("Done");
+	}
 }
